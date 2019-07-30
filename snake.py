a47f6d25d54a546d094290c5144f28df578beccd
barak21-meet <barak21@meet.mit.edu>
2019-07-30 14:30:47 +0300

    Stopping here

--- snake.py
+++ snake.py
@@ -75,24 +75,24 @@ LEFT_EDGE = -400
 
 def up():
     snake.direction="Up" #Change direction to up
-    move_snake() #Update the snake drawing 
+
     print("You pressed the up key!")
 
 #2. Make functions down(), left(), and right() that change snake.direction
 ####WRITE YOUR CODE HERE!!
 def down():
     snake.direction="Down"
-    move_snake()
+
     print("You pressed the down key")
 
 def left():
     snake.direction="Left"
-    move_snake()
+
     print("You pressed the left key")
 
 def right():
     snake.direction="Right"
-    move_snake()
+
     print("You pressed the rigt key")
       
 
@@ -106,6 +106,55 @@ turtle.onkeypress(right, "Right")
 
 turtle.listen()
 
+turtle.register_shape("trash.gif") #Add trash picture
+                      # Make sure you have downloaded this shape 
+                      # from the Google Drive folder and saved it
+                      # in the same folder as this Python script
+
+food = turtle.clone()
+food.shape("trash.gif") 
+
+#Locations of food
+food_pos = [(100,100), (-100,100), (-100,-100), (100,-100)]
+food_stamps = []
+
+# Write code that:
+#1. moves the food turtle to each food position
+#2. stamps the food turtle at that location
+#3. saves the stamp by appending it to the food_stamps list using
+# food_stamps.append(    )
+#4. Don't forget to hide the food turtle!
+for this_food_pos in food_pos :
+    food.goto(this_food_pos)
+    f_s=food.stamp()
+    food_stamps.append(f_s)
+    food.hideturtle()
+
+
+
+def make_food():
+    #The screen positions go from -SIZE/2 to +SIZE/2
+    #But we need to make food pieces only appear on game squares
+    #So we cut up the game board into multiples of SQUARE_SIZE.
+    min_x=-int(SIZE_X/2/SQUARE_SIZE)+1
+    max_x=int(SIZE_X/2/SQUARE_SIZE)-1
+    min_y=-int(SIZE_Y/2/SQUARE_SIZE)+1
+    max_y=int(SIZE_Y/2/SQUARE_SIZE)-1
+    
+    #Pick a position that is a random multiple of SQUARE_SIZE
+    food_x = random.randint(min_x,max_x)*SQUARE_SIZE
+    food_y = random.randint(min_y,max_y)*SQUARE_SIZE
+
+    food.goto(food_x,food_y)
+    f_p=food.pos()
+    food_pos.append(f_p)
+    n_f_s=food.stamp()
+    food_stamps.append(n_f_s)
+
+
+
+    
+    
 def move_snake():
     my_pos = snake.pos()
     x_pos = my_pos[0]
@@ -134,10 +183,70 @@ def move_snake():
 
     ######## SPECIAL PLACE - Remember it for Part 5
 
+    #If snake is on top of food item
+    if snake.pos() in food_pos:
+        food_index=food_pos.index(snake.pos()) #What does this do?
+        food.clearstamp(food_stamps[food_index]) #Remove eaten food stamp
+        food_pos.pop(food_index) #Remove eaten food position
+        food_stamps.pop(food_index) #Remove eaten food stamp
+        print("You have eaten the food!")
+    
+    #HINT: This if statement may be useful for Part 8
+
+    ...
+    #Don't change the rest of the code in move_snake() function:
+    #If you have included the timer so the snake moves 
+    #automatically, the function should finish as before with a 
+    #call to ontimer()
+    
+
+
+
+    
     #remove the last piece of the snake (Hint Functions are FUN!)
     remove_tail()
 
+    #Add new lines to the end of the function
+    #Grab position of snake
+    new_pos = snake.pos()
+    new_x_pos = new_pos[0]
+    new_y_pos = new_pos[1]
 
+    # The next three lines check if the snake is hitting the 
+    # right edge.
+    if new_x_pos >= RIGHT_EDGE:
+         print("You hit the right edge! Game over!")
+         quit()
+
+     # You should write code to check for the left, top, and bottom edges.
+    #####WRITE YOUR CODE HERE
+    if new_y_pos<=DOWN_EDGE:
+        print("You hit the bottom edge! Game over!")
+        quit()
+
+    if new_x_pos<=LEFT_EDGE:
+        print("You hit the left ende! Game over!")
+        quit()
+
+    if new_y_pos>=UP_EDGE:
+        print("You hit the top edge! Game over!")
+        quit()
+
+    if len(food_stamps)<=6:
+        make_food()
+
+    if snake.pos() in pos_list:
+        print("Your snake ate itself! Game over!")
+        quit()
+      
+    turtle.ontimer(move_snake,TIME_STEP) #<--- new line here
+
+
+
+
+
+
+move_snake()
 
 
 turtle.mainloop()
